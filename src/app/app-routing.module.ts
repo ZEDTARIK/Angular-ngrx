dde969e9ad532a0b8357896b792819f6ccbe91a2
ettarak <ettarak.zouhair@gmail.com>
2021-03-04 13:35:34 +0100

    Coding, Navbar , navigation Product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ProductsComponent } from './components/products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product', component: ProductsComponent },
+  { path: '', redirectTo: 'product', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
