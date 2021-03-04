2f8bc59788a410aef88fe648293ad5f38b0bd820
ettarak <ettarak.zouhair@gmail.com>
2021-03-04 12:05:13 +0100

    Coding, Generate Components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    ProductsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
