63c0c0b32303703a97331e0e7305c76766b0e569
ettarak <ettarak.zouhair@gmail.com>
2021-03-04 13:11:32 +0100

    Coding, Get Products Methods

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,31 @@
+import { environment } from './../../environments/environment';
+import { IProduct } from './../models/product';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  // http://localhost:3000/
+  host = environment.host;
+
+  constructor(private http: HttpClient) { }
+
+  // Get all Products from server 
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.host);
+  }
+
+  // Get all Products with query IsSelected = true 
+  getSelectedProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(`${this.host}/products?IsSelected=true`);
+  }
+
+  // Get all Products with query IsAvailable = true 
+  getAvailabeProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(`${this.host}/products?IsAvailable=true`);
+  }
 }
