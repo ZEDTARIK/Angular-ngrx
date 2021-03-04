c456107cd5327037f31f50b603b062b194fbdb26
ettarak <ettarak.zouhair@gmail.com>
2021-03-04 12:42:12 +0100

    Coding, Create interface Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductsComponent } from './components/products/products.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    Description: string;
+    Quantity?: number;
+    Price?: string;
+    IsSelected?: boolean;
+    IsAvailable?: boolean;
+    IsDeleted?: boolean;
+}
